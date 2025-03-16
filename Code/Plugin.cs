2d74231ca3df644217f3b60ad3aecb7fe5638d28
LordVGames <lordvgames810@gmail.com>
2025-03-15 23:55:33 -0400

    forgot to set it as client sided

--- Code/Plugin.cs
+++ Code/Plugin.cs
@@ -1,15 +1,17 @@
 using BepInEx;
 using RoR2;
+using R2API.Utils;
 
 namespace NegativeBossHealthTweak
 {
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     public class Plugin : BaseUnityPlugin
     {
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "LordVGames";
         public const string PluginName = "NegativeBossHealthTweak";
-        public const string PluginVersion = "1.0.0";
+        public const string PluginVersion = "1.0.1";
         public void Awake()
         {
             Log.Init(Logger);
--- Thunderstore/Package Files/CHANGELOG.md
+++ Thunderstore/Package Files/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+- Set mod as client-side (internally, not just the tag)
+
 ## 1.0.0
 
 - First release
